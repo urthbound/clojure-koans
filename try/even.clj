1e8f588a6ea1484148b1be37ddc02e7428b1f207
urthbound <jeffowler@gmail.com>
2009-02-20 21:38:53 -0800

    more clojure

---
+++ try/even.clj
@@ -0,0 +1,13 @@
+; (defn rec_is_even? [x]
+;   (if (= 0 x) true (if (= 0 (x - 2) false (rec_is_even (- x 2)))))
+; )
+
+; (println (rec_is_even? 0) )
+
+
+
+(println (* 4444 32))
+(* 4444 32)
+(* 444 2)
+(* 44 3)
+
