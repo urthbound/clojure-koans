7c41505406968c202f27c8542ce77ef6c550fec7
urthbound <jeffowler@gmail.com>
2014-02-19 13:41:33 -0500

    seq comprehensions

--- src/koans/09_runtime_polymorphism.clj
+++ src/koans/09_runtime_polymorphism.clj
@@ -6,20 +6,21 @@
                           (interpose ", " (concat (list a) more)))
                    "!")))
 
-(defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmulti diet (fn [x] (:name x)))
+(defmethod diet "Bambi" [a] "Bambi eats veggies.")
+(defmethod diet "Simba" [a] "Simba eats animals.")
+(defmethod diet "Thumper" [a] "Thumper eats veggies.")
+(defmethod diet :default [a] "I don't know what Rich Hickey eats.")
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
